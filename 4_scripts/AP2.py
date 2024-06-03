92e809e3fedf89d659055458b96d4e14d13acb5a
amandanere <amanda.nere@aluno.faculdadeimpacta.com.br>
2024-06-02 23:41:00 -0300

    Update AP2.py

--- 4_scripts/AP2.py
+++ 4_scripts/AP2.py
@@ -23,7 +23,7 @@ option = st.selectbox(
 #engine = sqa.create_engine("sqlite:///taxas.db", echo=True)
 #comn = engine.connect()
 #dados = pd.read_sql('taxas.db', con=comn)
-#df = pd.DataFrame(dados)
+df = pd.DataFrame(dados)
 #st.dataframe(df)
 
 tx = st.select_slider(
